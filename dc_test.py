2bd03f767b03bcfc37e173655eb728d3d79d1af0
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-12-07 19:50:02 +0000

    equal speed

--- dc_test.py
+++ dc_test.py
@@ -14,7 +14,7 @@ pwm.start(0)
 pwm2.start(0)
 try:
   while True:
-    pwm.ChangeDutyCycle(50)
+    pwm.ChangeDutyCycle(100)
     pwm2.ChangeDutyCycle(100)
     time.sleep(0.02)
 except KeyboardInterrupt:
