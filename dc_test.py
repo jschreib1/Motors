db072d96ab67bd29cc12065a646b4a05552cf798
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-12-07 19:45:10 +0000

    2 motors test

--- dc_test.py
+++ dc_test.py
@@ -2,15 +2,20 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 pwmPin = 16
+pin2 = 20
 GPIO.setup(pwmPin, GPIO.OUT)
+GPIO.setip(pin2, GPIO.OUT)
 
 dcMin = 0
 dcMax = 100
 pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
+pwm2 = GPIO.PWM(pin2, 50)
 pwm.start(0)
+pin2.start(0)
 try:
   while True:
     pwm.ChangeDutyCycle(50)
+    pin2.ChangeDutyCycle(10)
     time.sleep(0.02)
 except KeyboardInterrupt:
   print("bye")
