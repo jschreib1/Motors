e35d3acec9d23dedd6f4d2c8dd186d31f6658be5
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-12-07 19:46:39 +0000

    its pwm2 not pin 2 in the commands...

--- dc_test.py
+++ dc_test.py
@@ -11,11 +11,11 @@ dcMax = 100
 pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
 pwm2 = GPIO.PWM(pin2, 50)
 pwm.start(0)
-pin2.start(0)
+pwm2.start(0)
 try:
   while True:
     pwm.ChangeDutyCycle(50)
-    pin2.ChangeDutyCycle(10)
+    pwm2.ChangeDutyCycle(10)
     time.sleep(0.02)
 except KeyboardInterrupt:
   print("bye")
